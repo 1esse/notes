ba029cdc832b8454d7b41eb66188d6951c60f555
zhao-jiexin <zhaojiexin@hongjianguo.com>
2023-08-15 00:15:55 +0800

    update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -123,6 +123,7 @@ export default defineConfig({
           { text: '重积分', link: '/maths/multiple_integral.md' },
           { text: '微分方程', link: '/maths/equations.md' },
           { text: '多元函数', link: '/maths/multivariate_function.md' },
+          { text: '中值定理', link: '/maths/mean_value_theorem.md' },
         ]
       }, 
       {
